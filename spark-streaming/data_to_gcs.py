2048a8c5cda7d16b88781245c6e6686b012ae532
ghiles idris <96752546+ghiles10@users.noreply.github.com>
2023-03-06 15:26:08 +0000

    adding logging

--- spark-streaming/data_to_gcs.py
+++ spark-streaming/data_to_gcs.py
@@ -1,10 +1,15 @@
 from pyspark.sql import SparkSession
 from preprocess_stream_functions import read_kafka_streams, preprocess_stream
 import os 
+import logging_conf
+
+logger = logging_conf.logger
 
 ADRESS = os.environ['KAFKA_ADRESS'] 
 DATA_PATH = os.environ['DATA_PATH']
 CHECKPOINT_PATH = os.environ['CHECKPOINT_PATH']
+
+logger.debug("Creating spark session")
 
 spark = (SparkSession.builder
             .appName("finance")
@@ -12,8 +17,13 @@ spark = (SparkSession.builder
             .getOrCreate()
             )
 
+logger.debug(f"Reading data from kafka with {ADRESS} adress") 
 data_stream = read_kafka_streams(ADRESS, spark) 
+
+logger.debug(f" begin preprocess of data from kafka with {ADRESS} adress")
 data_stream_read = preprocess_stream(spark, data_stream= data_stream )
+
+logger.debug(f"Writing data to {DATA_PATH} path & {CHECKPOINT_PATH} checkpoint path")
 
 query = (data_stream_read
          .writeStream
@@ -23,7 +33,7 @@ query = (data_stream_read
          .partitionBy("year" , "month", "day", "hour")
          .option("path", DATA_PATH )
          .option("checkpointLocation", CHECKPOINT_PATH )
-         .trigger(processingTime="10 seconds")
+         .trigger(processingTime="50 seconds")
          .start() )
 
 query.awaitTermination()
--- spark-streaming/preprocess_stream_functions.py
+++ spark-streaming/preprocess_stream_functions.py
@@ -1,10 +1,15 @@
 from pyspark.sql.functions import expr, from_json, month, hour, dayofmonth, col, year, split
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType, LongType
 import time
+import logging_conf   
+
+logger = logging_conf.logger
 
 def read_kafka_streams(adress,spark, topic = 'finance'): 
 
   # Define the input data stream
+  logger.debug("Reading data from kafka topic: {}".format(topic)) 
+               
   data_stream = (spark.readStream
                 .format("kafka")
                 .option("kafka.bootstrap.servers", adress)
@@ -38,8 +43,10 @@ def preprocess_stream(spark, data_stream,  topic = "finance", output_mode="appen
     StructField("makerCoefficient", DoubleType())
   ])
 
-
+  
   # Transform the value column from string to JSON and select the fields
+  logger.debug("Transforming data from kafka topic: {}".format(topic)) 
+
   data_stream_json = (data_stream
                       .select(from_json(expr("CAST(value AS STRING)"), json_schema).alias("json_data"))
                       .selectExpr("json_data.time", "json_data.symbol", "json_data.buy", "json_data.sell", "json_data.changeRate",
@@ -49,6 +56,8 @@ def preprocess_stream(spark, data_stream,  topic = "finance", output_mode="appen
                                   )
 
   # transofmre the time column to timestamp and add year, month, day and hour columns
+  logger.debug(f"Adding year, month, day and hour columns to the data from kafka topic: {topic}")
+
   data_stream_json=  ( data_stream_json.withColumn("time", (col("time")/1000).cast("timestamp") )
                                     .withColumn("year", year("time")) 
                                     .withColumn("month", month("time"))
@@ -57,6 +66,7 @@ def preprocess_stream(spark, data_stream,  topic = "finance", output_mode="appen
                                             )                                
 
   data_stream_json =  data_stream_json.withColumn("symbol", split(data_stream_json["symbol"], "-")[0]) 
+  logger.debug("end of preprocessing from kafka topic: {}".format(topic)
 
   return data_stream_json
 
